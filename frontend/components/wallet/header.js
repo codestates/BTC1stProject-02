c2625b041075462d8533b47e915a74fab83fdb0f
hyun2 <hks0284@gmail.com>
2022-02-26 17:49:05 +0900

    feat: 지갑 제거 기능 추가

--- frontend/components/wallet/header.js
+++ frontend/components/wallet/header.js
@@ -1,4 +1,5 @@
 import styled from "@emotion/styled";
+import { Tooltip } from "@mantine/core";
 import { useStore } from "../../utils/store";
 
 const Container = styled.div`
@@ -15,7 +16,10 @@ const RemoveWallet = styled.div`
 `;
 
 const WalletHeader = () => {
-  const setUser = useStore((state) => state.setUser);
+  const [setUser, setActiveTab] = useStore((state) => [
+    state.setUser,
+    state.setActiveTab,
+  ]);
 
   const handleClickLock = () => {
     let user = localStorage.getItem("user");
@@ -33,8 +37,26 @@ const WalletHeader = () => {
       <div
         style={{ flex: "1", display: "flex", justifyContent: "space-between" }}
       >
-        <LockButotn onClick={handleClickLock}>잠금</LockButotn>
-        <RemoveWallet>지갑 제거</RemoveWallet>
+        <Tooltip
+          label="지갑을 잠근 후 비밀번호를 이용해 다시 잠금해제 할 수 있습니다."
+          withArrow
+        >
+          <LockButotn onClick={handleClickLock}>잠금</LockButotn>
+        </Tooltip>
+        <Tooltip
+          label="지갑을 제거합니다. 제거 후에도 개인키로 다시 지갑을 불러올 수 있습니다."
+          withArrow
+        >
+          <RemoveWallet
+            onClick={() => {
+              localStorage.removeItem("user");
+              setUser(null);
+              setActiveTab("CREATE_WALLET");
+            }}
+          >
+            지갑 제거
+          </RemoveWallet>
+        </Tooltip>
       </div>
     </Container>
   );
